f086a23b14d7a4de64bac2e5ca45e6fac29ba09c
wasantac <34143200+wasantac@users.noreply.github.com>
2021-09-13 00:23:50 -0500

    calendario

--- src/Views/Calendario.js
+++ src/Views/Calendario.js
@@ -1,10 +1,81 @@
 import React from 'react';
-import {Container} from 'react-bootstrap'
+import {Container,Form,Button} from 'react-bootstrap'
+import axios from 'axios';
+import Swal from 'sweetalert2';
 const Calendario = () => {
+    let handleSubmit = (e) => {
+        let lunes = e.target[0].value;
+        let martes = e.target[1].value;
+        let miercoles = e.target[2].value;
+        let jueves = e.target[3].value;
+        let viernes = e.target[4].value;
+        let sabado = e.target[5].value;
+        let domingo = e.target[6].value;
+        axios.post('/calendario',{
+            lunes,
+            martes,
+            miercoles,
+            jueves,
+            viernes,
+            sabado,
+            domingo,
+            token : localStorage.getItem('token')
+        }).then(res => {
+            Swal.fire(
+                'Calendario Actualizado',
+                `Se ha actualizado el calendario de la semana`,
+                'success'
+              ).then(() => {
+                  window.location.reload()
+              })
+        }).catch(err => {
+            Swal.fire(
+                'Error',
+                `${err}`,
+                'error'
+              ).then(() => {
+                  window.location.reload()
+              })
+        }) 
+        e.preventDefault();
+    }
     return (
         <Container>
             <h1 className="text-center">Calendario</h1>
-            <p className="text-center">En construcción...</p>
+            <Form className="card p-5 shadow" onSubmit={handleSubmit}>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Lunes</Form.Label>
+                    <Form.Control as="textarea" rows={3}  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Martes</Form.Label>
+                    <Form.Control as="textarea" rows={3}  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Miércoles</Form.Label>
+                    <Form.Control as="textarea" rows={3}  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Jueves</Form.Label>
+                    <Form.Control as="textarea" rows={3}  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Viernes</Form.Label>
+                    <Form.Control as="textarea" rows={3}  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Sábado</Form.Label>
+                    <Form.Control as="textarea" rows={3}  />
+                </Form.Group>
+                <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
+                    <Form.Label>Domingo</Form.Label>
+                    <Form.Control as="textarea" rows={3}  />
+                </Form.Group>
+                <div className="d-flex align-items-center justify-content-center flex-column">
+                <Button type="submit" >Actualizar Calendario
+                </Button>
+                </div>
+            </Form>
         </Container>
     );
 }
